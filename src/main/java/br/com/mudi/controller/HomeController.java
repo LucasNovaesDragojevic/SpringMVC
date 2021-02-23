676fd1cf11a8c76cb8ad8e188fbab9355f043fbd
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2021-02-23 01:03:31 -0300

    Implementada conexão com a base de dados

--- src/main/java/br/com/mudi/controller/HomeController.java
+++ src/main/java/br/com/mudi/controller/HomeController.java
@@ -1,27 +1,25 @@
 package br.com.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import br.com.mudi.model.Pedido;
+import br.com.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController 
 {
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
 	@GetMapping("/home")
 	public String home(Model model)
 	{
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Kindle Oasis");
-		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/61SVKnucPtL._AC_SL1001_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Novo-Kindle-Oasis-8Gb-temperatura-de-luz-ajust%C3%A1vel/dp/B07L57H5X4");
-		pedido.setDescricao("E-Reader");
-		
-		List<Pedido> pedidos = Arrays.asList(pedido);
+		List<Pedido> pedidos = pedidoRepository.findAll();
 		
 		model.addAttribute("pedidos", pedidos);
 		
--- src/main/java/br/com/mudi/model/Pedido.java
+++ src/main/java/br/com/mudi/model/Pedido.java
@@ -3,12 +3,21 @@ package br.com.mudi.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter @Setter
 public class Pedido 
 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	private String nomeProduto;
 	private BigDecimal valorNegociado;
 	private LocalDate dataEntrega;
