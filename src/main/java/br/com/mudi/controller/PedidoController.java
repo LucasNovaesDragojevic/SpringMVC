5ea8abd35201c602e893ef3c4fc2861d81eb6370
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2021-02-24 19:57:17 -0300

    Criadas páginas públicas e restritas

--- src/main/java/br/com/mudi/controller/PedidoController.java
+++ src/main/java/br/com/mudi/controller/PedidoController.java
@@ -3,6 +3,7 @@ package br.com.mudi.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.mudi.dto.NovoPedidoRequest;
 import br.com.mudi.model.Pedido;
+import br.com.mudi.model.User;
 import br.com.mudi.repository.PedidoRepository;
+import br.com.mudi.repository.UserRepository;
 
 @Controller
 @RequestMapping("pedido")
@@ -19,6 +22,9 @@ public class PedidoController
 {
 	@Autowired
 	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	@GetMapping("formulario")
 	public String formulario(NovoPedidoRequest novoPedidoRequest) 
@@ -33,7 +39,10 @@ public class PedidoController
 		{
 			return "/pedido/formulario";
 		}
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+		User user = userRepository.findByUsername(username);
 		Pedido pedido = novoPedidoRequest.toPedido();
+		pedido.setUser(user);
 		pedidoRepository.save(pedido);
 		return "redirect:/home";
 	}
--- src/main/java/br/com/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/mudi/repository/PedidoRepository.java
@@ -2,7 +2,11 @@ package br.com.mudi.repository;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.mudi.model.Pedido;
@@ -11,5 +15,12 @@ import br.com.mudi.model.StatusPedido;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>
 {
-	List<Pedido> findByStatusPedido(StatusPedido statusPedido);
+	@Cacheable("pedidos")
+	List<Pedido> findByStatusPedido(StatusPedido statusPedido, Pageable pageable);
+
+	@Query("select p from Pedido p join p.user u where u.username = :username")
+	List<Pedido> findAllByUsuario(@Param("username") String username);
+
+	@Query("select p from Pedido p join p.user u where u.username = :username and p.statusPedido = :status")
+	List<Pedido> findByStatusEUsuario(@Param("status") StatusPedido status, @Param("username") String user);
 }
