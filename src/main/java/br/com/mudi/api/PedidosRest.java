a39d99d39c27dc2bcdfd7008e02297f9bb6251ce
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2021-02-24 20:17:29 -0300

    Criado controller rest

---
+++ src/main/java/br/com/mudi/api/PedidosRest.java
@@ -0,0 +1,31 @@
+package br.com.mudi.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.mudi.model.Pedido;
+import br.com.mudi.model.StatusPedido;
+import br.com.mudi.repository.PedidoRepository;
+
+@RestController
+@RequestMapping("api/pedidos")
+public class PedidosRest
+{
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@GetMapping("aguardando")
+	public List<Pedido> getPedidoAguardandoOfertas()
+	{
+		Sort sort = Sort.by("id").descending();
+		PageRequest pageRequest = PageRequest.of(0, 3, sort);
+		List<Pedido> pedidos = pedidoRepository.findByStatusPedido(StatusPedido.AGUARDANDO, pageRequest);
+		return pedidos;
+	}
+}
